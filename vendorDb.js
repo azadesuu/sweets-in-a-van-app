a418bc69f7f86ecd5e2d34c7a9b35fde61aea833
Yue <ytten@student.unimelb.edu.au>
2021-04-08 00:37:05 +1000

    add html file and basic route

--- vendorDb.js
+++ vendorDb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-let connectionURL = 'mongodb+srv://<username>:<password>@info30005-1.xpxvw.mongodb.net/app-server?retryWrites=true&w=majority'
-mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, dbName: 'info30005-1'})
+let connectionURL = 'mongodb+srv://anything30005:F5Nruod8fTvdNTCz@info30005-1.xpxvw.mongodb.net/app-server'
+mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, dbName: 'app-server'})
 const db = mongoose.connection
 
 //event handler
@@ -9,19 +9,23 @@ db.once('open', () => {
     console.log('connected to Mongo')
 })
 
-const vendorSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    photo: String,
-    description: String,
-    location: {type: Number}
+const locationSchema = new mongoose.Schema({
+    latitude:{type: Number},
+    longtitude:{type: Number}
 })
 
 const orderSchema = new mongoose.Schema({
     id: {type: Number, required: true, unique: true},
+    isFulfilled: {type: Boolean, default:false}
 })
 
+const vendorSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    isReadyForOrders:{type: Boolean, default: true},
+    location: [locationSchema]
+})
 
+const location = mongoose.model('Location', locationSchema)
 const vendor = mongoose.model('Vendor', vendorSchema)
 const order = mongoose.model('Order',orderSchema)
-
-module.exports = {vendor, order}
+module.exports = {vendor, order, location}
