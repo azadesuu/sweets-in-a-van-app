3fffe74e52ef80684d13b618062f2dab35bd3a98
Yue <ytten@student.unimelb.edu.au>
2021-04-14 23:53:48 +1000

    using handle bar

--- vendor/vendorApp.js
+++ vendor/vendorApp.js
@@ -3,28 +3,26 @@ const express = require('express')
 const app = express()
 // process.env.PORT ||
 const port =  8080
-var path = require('path')
-// const bodyParser = require('body-parser')
-// const exphbs = require('express-handlebars')
-app.use(express.json())  // replaces body-parser
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+//app.use(express.json())  // replaces body-parser
 
 
 //Mongoose stuff
 const {vendor} = require('./vendorDb.js')
 const {order} = require('./vendorDb.js')
 //hbs
-// app.engine('hbs', exphbs({
-// 	defaultlayout: 'main',
-// 	extname: 'hbs'
-// }))
+app.engine('hbs', exphbs({
+	defaultlayout: 'main',
+	extname: 'hbs'
+}))
 
-// app.set('view engine', 'hbs')
-
-// app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({extended:true}))
 
 //home 
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/static/menu.html'))
+    res.render('index')
 })
 
 //outstandingorders 
@@ -34,19 +32,19 @@ app.get('/allOutstandingOrders', async(req, res)=> {
 })
 //setting van location
 app.get('/setLocation', async(req, res)=>{
-    res.sendFile(path.join(__dirname, '/static/setVanLocation.html'))
+    res.render('setVanLocation')
 })
 
-// app.post('/setLocation', async(req, res)=>{
-//     const newLocation = new Location({
-//         latitude: req.body.latitude,
-//         longtitude: req.body.longtitude
-//     })
-//     newLocation.save((err, result)=>{
-//         if(err) res.send(err)
-//         return res.send(result)
-//     })
-// })
+app.post('/insertStatus', async(req, res)=>{
+    const newLocation = new Location({
+        latitude: req.body.latitude,
+        longtitude: req.body.longtitude
+    })
+    newLocation.save((err, result)=>{
+        if(err) res.send(err)
+        return res.send(result)
+    })
+})
 
 // start the server listening
 app.listen(port, () => {
