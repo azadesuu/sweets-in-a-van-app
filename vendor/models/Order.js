1cafa19f6e90e607acd340dc75a6051e00d68a10
ytten <62542262+ytten@users.noreply.github.com>
2021-04-19 20:09:57 +1000

    Delete vendor directory

--- vendor/models/Order.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const orderSchema = new mongoose.Schema({
-    id: {type: Number, required: true, unique: true},
-    isFulfilled: {type: Boolean, default:false}
-},{collection: 'orders'})
-
-const Order = mongoose.model('orders',orderSchema)
-module.exports = {Order, orderSchema}
