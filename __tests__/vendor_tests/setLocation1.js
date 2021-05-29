55595fc86d6861a24a67ec76b13441048633e86b
chebc <chebc@student.unimelb.edu.au>
2021-05-30 01:14:30 +0800

    add comments in __test__ and guidence txt file

--- __tests__/vendor_tests/setLocation1.js
+++ __tests__/vendor_tests/setLocation1.js
@@ -1,29 +1,25 @@
-
 const request = require('supertest')
-
+//need export app from app.js 
 const app = require('../../app');
-//const { TestWatcher } = require('@jest/core');
 
+/*------------------------------------This is Integration test part--------------------------------- */
 describe('Integration test: set locations',()=>{
+    //create agent and cookie
     let agent = request.agent(app)
-
     let cookie = null;
 
-
+ 
     beforeAll(()=>agent
-    
-    .post('/vendor/login')
-    
+    //send a POST request to login
+    .post('/vendor/login')  
     .set('Content-Type', 'application/x-www-form-urlencoded')
-
+    //send van_first_name, van_last_name and locDescription
     .send({
         van_first_name: 'Backdoor',
         van_last_name:'Festival',
         locDescription:'5644',
     })
-
-
-
+    //get the back cookie and store it
     .then((res)=>{
         cookie = res
             .headers['set-cookie'][0]
@@ -32,7 +28,7 @@ describe('Integration test: set locations',()=>{
             .join(';')
     }));
 
-
+    //Test Case 1: Trying to get log in and access to vendor page
     test('Test 1 (valid login)',() => {
         return agent
 
@@ -45,15 +41,15 @@ describe('Integration test: set locations',()=>{
 
     })
 
-
+    //Test Case 2: Trying to get log in and access to order page
     test('Test 2 (go to setLocation)',() => {
         return agent
 
-        .get('/vendor/VAN11/setLocation')
+        .get('/vendor/VAN11/orders')
 
         .set('Cookie',cookie)
         .then((response)=>{
-            expect('/vendor/VAN11/setLocation');
+            expect('/vendor/VAN11/orders');
             
         })
 
