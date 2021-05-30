d58c7a9d9248e330357ff20922fab1e76a0c663c
Zixuan Liu <zixliu@student.unimelb.edu.au>
2021-05-30 21:10:10 +1000

    15 min to 10 min

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -318,7 +318,7 @@ const getOrderDetail = async(req,res)=>{
             const van = await Vendor.findOne( {van_ID: orderRaw.van_ID}).lean();
             var timeCreated = orderRaw.when;
             timeCreated = timeCreated.getTime() / 1000;
-            var timeRemaining = 900 - (Date.now()/1000 - timeCreated);
+            var timeRemaining = 600 - (Date.now()/1000 - timeCreated);
             var hasTimeLeft = false;
             if (timeRemaining > 0) {
                 hasTimeLeft = true;
